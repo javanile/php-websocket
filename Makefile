01452bb1a5956d0919ff5f325653e5a122008ec8
francesco <info.francescobianco@gmail.com>
2023-10-30 17:42:00 +0100

    First commit

--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
 
-
-
-test:
-	@php -f websocket.php
-
 require:
 	@composer require --dev phrity/websocket
 	@composer require --dev phpunit/phpunit ^10
 	@composer require --dev symfony/process
+
+update:
+	@composer update
+
+test:
+	@php -f websocket.php
 
 test-websocket:
 	@./vendor/bin/phpunit tests/WebsocketTest.php
--- tests/WebsocketTest.php
+++ tests/WebsocketTest.php
@@ -2,18 +2,33 @@
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
+use WebSocket\Client;
 
 final class WebsocketTest extends TestCase
 {
     public function testCanBeCreatedFromValidEmail()
     {
-        $process = new Process(['php', '-f', 'websocket.php', '--', '0.0.0.0', '40000']);
+        $server = new Process(['php', '-f', 'websocket.php', '--', '0.0.0.0', '40000']);
 
-        $process->start();
+        $server->start();
 
-        $this->assertEquals('Cavallo', $process->getOutput());
+        sleep(1);
 
-        $process->stop();
+        $this->assertEquals('{"message":"Server started on 0.0.0.0:40000."}', trim($server->getOutput()));
+
+        $client = new Client('ws://0.0.0.0:40000/');
+
+        $client->send(json_encode([
+            'user' => 'user',
+            'app' => 'app',
+            'session' => md5('user|app'),
+        ]));
+
+        $response = $client->receive();
+
+        $this->assertStringContainsString('Welcome to the PHP WebSocket Server', $response);
+
+        $server->stop();
     }
 }
 
